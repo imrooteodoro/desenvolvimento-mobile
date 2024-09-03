37faf2699b0776dc58ed1aec9ba0492c6c6a96fc
imrooteodoro <adelsonteodoro67@gmail.com>
2024-09-03 19:21:58 -0300

    feat: adiciona pagamento

--- src/POO/engenharia_de_software/pagamento.dart
+++ src/POO/engenharia_de_software/pagamento.dart
@@ -8,5 +8,38 @@ class Pagamento{
     String? _date;
     String? _metodo;
     String? _status;
+    bool? _pago;
+
+
+    Pagamento(valor,date, metodo, status, pago){
+      this._valor = valor;
+      this._date = date;
+      this._metodo = metodo;
+      this._status = status;
+      this._pago = pago;
+
+    }
+
+    String? processar(){
+      if( _valor != 0){
+        return 'Você pode processar o valor(transferir, etc)';
+      }
+      else{
+        return 'Desculpe, mas vocês não possui saldo suficiente para fazer transações';
+      }
+    }
+
+    String? cancelar(){
+      return 'Ação cancelada com sucesso';
+    }
+
+    double? reembolsar(valor_reembolso){
+       double? reembolso = valor_reembolso + _valor;
+      return reembolso ;
+    }
+
+    String? verificarStatus(){
+      return 'Verificando Status de pagamento';
+    }
 
 }
